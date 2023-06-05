46031f16f4e3d406937cfb3545bc965c3e0edd9c
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-06-05 17:53:09 -0300

    req. 1 ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    students_quantity_by_hour = 0
+    for student_permanence in permanence_period:
+        a, b = student_permanence
+        if type(a) != int or type(b) != int or type(target_time) != int:
+            return None
+        else:
+            if target_time in range(a, b + 1):
+                students_quantity_by_hour += 1
+    return students_quantity_by_hour
