d1bc7229ed097ac4e92fe93076908164d5d82e5a
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-06-05 19:41:57 -0300

    req. 3 ok

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    if low_index == (len(word) // 2) + 1:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
