1cd4ee87bae22bec28b25eeab929560925823d49
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-06-05 18:30:46 -0300

    req. 2 ok

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # caso a função receba tipos incorretos nos parâmetros
+    with pytest.raises(TypeError):
+        encrypt_message(0, "string")
+
+    # caso a função receba um índice inválido na key
+    assert encrypt_message("fail", -1) == "liaf"
+
+    # caso a função receba uma key ímpar
+    assert encrypt_message("fail", 1) == "f_lia"
