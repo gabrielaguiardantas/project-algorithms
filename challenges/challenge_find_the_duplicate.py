afd44d0839479fc54f1326ceb0eb4af44a292f74
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-06-06 17:44:30 -0300

    req. 5 ok

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,22 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not validate_nums(nums):
+        return False
+    nums.sort()
+    i = 0
+    for number in nums[:-1]:
+        if number == nums[i + 1]:
+            return number
+        i += 1
+    return False
+
+
+def validate_nums(nums):
+    if len(nums) <= 1:
+        return False
+    for number in nums:
+        if type(number) != int:
+            return False
+        if number < 0:
+            return False
+    return True
