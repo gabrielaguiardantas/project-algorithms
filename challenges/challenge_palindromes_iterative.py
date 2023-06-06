db613215039c7e5d60fe8f2a36bd10de48208e1e
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-06-06 18:21:13 -0300

    req. 6 ok

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    i = 0
+    if len(word) == 0:
+        return False
+    for letter in word:
+        if letter == word[i - 1]:
+            i -= 1
+        else:
+            return False
+    return True
